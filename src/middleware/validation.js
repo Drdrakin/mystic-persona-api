9b0ab5c1f41102a972ecb08457a1f123b021ad17
Guilherme Alves <drdrakino@gmail.com>
2024-11-25 18:01:38 -0300

    feat: middleware de validação mongo, .env atualizado

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,16 +1,16 @@
 import AvatarPart from '../models/AvatarPart.js';
 
-const  validateAvatarParts = async (req, res, next)  => {
-  const { avatarParts } = req.body;
-  try {
-    const partIds = avatarParts.map(part => part.part);
-    const validParts = await AvatarPart.find({ _id: { $in: partIds } });
+const validateAvatarParts = async (req, res, next)  => {
+        const { avatarParts } = req.body;
+    try {
+        const partIds = avatarParts.map(part => part.part);
+        const validParts = await AvatarPart.find({ _id: { $in: partIds } });
     if (validParts.length !== avatarParts.length) {
-      return res.status(400).json({ message: 'Some avatar parts are invalid.' });
+        return res.status(400).json({ message: 'Some avatar parts are invalid.' });
     }
     next();
   } catch (err) {
-    res.status(500).json({ message: 'Error validating avatar parts' });
+    res.status(500).json({ message: 'Error validating avatar parts', error: err });
   }
 }
 
